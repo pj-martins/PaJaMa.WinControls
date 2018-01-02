6d643c0f5fd7a75f75d5e3713a879421a8d05b6f
pj_martins@outlook.com <pj_martins@outlook.com>
2018-01-02 14:22:18 -0800

    scrollable message box

---
+++ ScrollableMessageBox.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace PaJaMa.WinControls
+{
+	partial class ScrollableMessageBox
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnOK = new System.Windows.Forms.Button();
+			this.txtLines = new System.Windows.Forms.TextBox();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// btnOK
+			// 
+			this.btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.btnOK.Location = new System.Drawing.Point(415, 6);
+			this.btnOK.Name = "btnOK";
+			this.btnOK.Size = new System.Drawing.Size(75, 23);
+			this.btnOK.TabIndex = 0;
+			this.btnOK.Text = "OK";
+			this.btnOK.UseVisualStyleBackColor = true;
+			this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+			// 
+			// txtLines
+			// 
+			this.txtLines.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.txtLines.BackColor = System.Drawing.Color.White;
+			this.txtLines.BorderStyle = System.Windows.Forms.BorderStyle.None;
+			this.txtLines.Location = new System.Drawing.Point(12, 12);
+			this.txtLines.Multiline = true;
+			this.txtLines.Name = "txtLines";
+			this.txtLines.ReadOnly = true;
+			this.txtLines.Size = new System.Drawing.Size(471, 287);
+			this.txtLines.TabIndex = 1;
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.SystemColors.Control;
+			this.panel1.Controls.Add(this.btnOK);
+			this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+			this.panel1.Location = new System.Drawing.Point(0, 305);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(495, 35);
+			this.panel1.TabIndex = 2;
+			// 
+			// ScrollableMessageBox
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.White;
+			this.ClientSize = new System.Drawing.Size(495, 340);
+			this.Controls.Add(this.panel1);
+			this.Controls.Add(this.txtLines);
+			this.Name = "ScrollableMessageBox";
+			this.ShowIcon = false;
+			this.ShowInTaskbar = false;
+			this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+			this.Load += new System.EventHandler(this.ScrollableMessageBox_Load);
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnOK;
+		private System.Windows.Forms.TextBox txtLines;
+		private System.Windows.Forms.Panel panel1;
+	}
+}
---
+++ ScrollableMessageBox.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PaJaMa.WinControls
+{
+	public partial class ScrollableMessageBox : Form
+	{
+		public ScrollableMessageBox()
+		{
+			InitializeComponent();
+		}
+
+		public static DialogResult ShowDialog(string[] lines, string caption = "")
+		{
+			return show(lines, caption, true);
+		}
+
+		public static void Show(string[] lines, string caption = "")
+		{
+			show(lines, caption, false);
+		}
+
+		private static DialogResult show(string[] lines, string caption, bool isDialog)
+		{
+			var msg = new ScrollableMessageBox();
+			msg.txtLines.Lines = lines;
+			msg.Text = caption;
+			var height = lines.Length * 13;
+			if (height > 800)
+			{
+				height = 800;
+				msg.txtLines.ScrollBars = ScrollBars.Vertical;
+			}
+			msg.Height = (msg.Height - msg.txtLines.Height) + height + 7;
+			if (isDialog)
+				return msg.ShowDialog();
+			msg.Show();
+			return DialogResult.None;
+		}
+
+		private void btnOK_Click(object sender, EventArgs e)
+		{
+			this.DialogResult = DialogResult.OK;
+			this.Close();
+		}
+
+		private void ScrollableMessageBox_Load(object sender, EventArgs e)
+		{
+			btnOK.Select();
+		}
+	}
+}
